c6834ed9689e952d629551fbe60b7cf8538f02da
Aryan Nagardhankar <aryannagardhankar2020@gmail.com>
2026-02-05 12:07:17 +0530

    PORT

--- src/main/java/com/resturants/config/CorsConfig.java
+++ src/main/java/com/resturants/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins(
-                            "http://localhost:5176",
+                            "http://localhost:5173",
                             "https://elite-restaurent.vercel.app"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
