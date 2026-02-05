358f64a4eb1a69e681d34e7b0e863e53d1ff6efc
Aryan Nagardhankar <aryannagardhankar2020@gmail.com>
2026-02-05 10:32:17 +0530

    First

---
+++ src/test/java/com/resturants/ResturantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resturants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResturantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
